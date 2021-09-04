cb36e7dbb7f7348e7fc3b60955a84593629de62d
Mukul Kumar <81973654+Mukul-08@users.noreply.github.com>
2021-09-04 21:32:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 60*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
+
+let a;
+let date;
+let time;
+setInterval(() => {
+ a = new Date();
+ date = a.toLocaleDateString();
+ time = a.getHours() + ' : ' + a.getMinutes() + ' : ' + a.getSeconds();
+document.getElementById('date').innerHTML = date;
+document.getElementById('time').innerHTML = time; 
+document.getElementById("aud").play();
+
+}, 1000);
